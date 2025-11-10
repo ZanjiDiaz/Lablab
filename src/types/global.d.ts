2e7568439f88635c861b619162163840f1c4651d
Jizan Diaz Villafuerte <diazjizan@gmail.com>
2025-11-10 19:32:56 +0800

    Fix

--- src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
-}
-
-declare module '*.otf';
-declare module '*.ttf';
-declare module '*.woff';
-declare module '*.woff2';
